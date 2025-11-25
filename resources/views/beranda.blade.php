6e9df468c6ccc9493b9f062fdffa99b2073a8938
Chalifahdien <alifhamud.2004@gmail.com>
2025-11-25 12:41:22 +0800

    babi diksan 13

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -83,7 +83,7 @@
                     </a>
                 </div>
                 <div class="col-6 col-md-3 p-0">
-                    <a href="#" class="card card-link card-link-pop bg-transparent rounded-0 py-3">
+                    <a href="/virtual-tour" class="card card-link card-link-pop bg-transparent rounded-0 py-3">
                         <div class="card-body">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
@@ -100,7 +100,8 @@
                     </a>
                 </div>
                 <div class="col-6 col-md-3 p-0">
-                    <a href="#" class="card card-link card-link-pop bg-transparent rounded-0 py-3">
+                    <a href="https://www.roblox.com/games/104634170739997/" target="_blank"
+                        class="card card-link card-link-pop bg-transparent rounded-0 py-3">
                         <div class="card-body">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"

9c6f9663ed7c0cf9c2599829b400120071b08483
Chalifahdien Hamud <hamimu@Chalifahdien-Hamud.local>
2025-11-25 23:53:45 +0800

    babi diksan 19

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Museum Bharugano Wuna</title>
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="{{ asset('img/logo.png') }}">
+
     <!-- CSS files -->
     <link href="{{ asset('dist/css/tabler.min.css') }}" rel="stylesheet" />
-    {{-- <link href="{{ asset('dist/css/tabler-flags.min.css') }}" rel="stylesheet" /> --}}
-    {{-- <link href="{{ asset('dist/css/tabler-socials.min.css') }}" rel="stylesheet" /> --}}
-    {{-- <link href="{{ asset('dist/css/tabler-payments.min.css') }}" rel="stylesheet" /> --}}
     <link href="{{ asset('dist/css/tabler-vendors.min.css') }}" rel="stylesheet" />
     <link href="{{ asset('dist/css/tabler-marketing.min.css') }}" rel="stylesheet" />
-    {{-- <link href="{{ asset('dist/css/demo.min.css') }}" rel="stylesheet" /> --}}
     <style>
         @import url('https://rsms.me/inter/inter.css');
 
